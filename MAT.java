f4f72511530505ee1c4460e23283ec30cbd57fec
NatanM4 <136546061+Handaxton@users.noreply.github.com>
2023-11-24 20:36:32 -0400

    Update MAT.java

--- MAT.java
+++ MAT.java
@@ -1,13 +1,14 @@
 public class MAT {
     
     public static void main(String[] args) {
-       
+        // Criando a matriz de 5 linhas e 10 colunas
         int[][] MAT = new int[5][10];
 
-       
+        // Preenchendo a matriz com números inteiros (pode adaptar conforme necessário)
+        // Aqui, estou preenchendo com valores aleatórios para demonstração
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 10; j++) {
-                MAT[i][j] = (int) (Math.random() * 100); 
+                MAT[i][j] = (int) (Math.random() * 100); // Números aleatórios entre 0 e 99 
             }
         }
 
@@ -21,7 +22,7 @@ public class MAT {
             System.out.println("Linha " + (i + 1) + ": Soma = " + somaLinha + ", Paridade = " + (somaLinha % 2 == 0 ? "Par" : "Ímpar"));
         }
 
-       
+        // 1. Soma dos elementos de cada linha e verificação par ou ímpar
         System.out.println("\nSoma dos elementos de cada coluna e paridade:");
         for (int j = 0; j < 10; j++) {
             int somaColuna = 0;
